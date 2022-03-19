d13cb170a3d5af31f84f994490b357ce7a348def
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-19 13:45:13 -0700

    commit tree 63a993ae0838d5dd95d0f291afc7cdcc54e8e3de

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const element = document.getElementById("main");
-    element.remove("main");
+const main = document.getElementById('main');
+    main.remove();
 
-const newHeader = document.createElement("h1");
+const newHeader = document.createElement('h1');
     newHeader.id = "victory";
 
 newHeader.textContent = "Carson is the champion";
-    document.body.append(newHeader);
+document.body.append(newHeader);
